a2c00f467fd52022910c49c047ac0dce4e4c98b8
almunimax <almunimax@mail.ru>
2022-12-16 23:53:07 +0300

    string dz

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Задание 1");
+
+        String firstName = "ivan";
+        String middleName = "Ivanovich";
+        String lastName = "ivanov";
+        String fullName = lastName +" "+ firstName +" "+ middleName;
+        System.out.println("ФИО сотрудника-" + fullName);
+
+
+    }
+}
